cb7e8392a43d80bd85fda5b6e6e13edecda15ce2
Crowdbotics <team@crowdbotics.com>
2021-09-20 21:38:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "boss_planner_30713Identifier",
+  url: "https://boss_planner_30713Identifier.botics.co",
+  api: "https://boss_planner_30713Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
